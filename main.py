3d7cf3136bd5ee7a2eef4dc48dae94b72fc701e4
Iftach Naftaly <iftahnaf@gmail.com>
2022-04-17 12:06:22 +0300

    offline planner works, need to tune parameters

--- main.py
+++ main.py
@@ -6,10 +6,8 @@ def main():
     scenario = Scenario()
     map = scenario.map1
     planner = CoveragePathPlanner(map)
-    distance_map = planner.calculate_distance_map()
-    print(distance_map)
-    x, y, steps = planner.plan()
-    # scenario.draw_map(map, x, y)
+    x, y, steps = planner.off_line_planning()
+    scenario.draw_map(map, x, y)
     print(f"Done in {steps} steps")
 
 if __name__ =="__main__":
--- modules/CoveragePathPlanner.py
+++ modules/CoveragePathPlanner.py
@@ -1,3 +1,4 @@
+import dis
 from re import M
 import numpy as np
 import itertools
@@ -36,7 +37,7 @@ class CoveragePathPlanner():
                 counter = counter + 1         
         return counter
 
-    def check_neighbors(self, pose, route, repeat_num=0):
+    def check_neighbors(self, pose, route, repeat_num=0, dist_map=None):
         # this is the order of the movment list directions
         right = [pose[0] + 0, pose[1] + 1]
         left = [pose[0] + 0, pose[1] - 1]
@@ -46,19 +47,25 @@ class CoveragePathPlanner():
         neighbors = [right, left, up, down]
 
         movements = []
+        distances = []
 
         for neighbor in neighbors:
             if self.check_bounderies(neighbor):
                 movements.append(0)
+                distances.append(0)
                 continue
             if self.check_obstacle(neighbor):
                 movements.append(0)
+                distances.append(0)
                 continue
             if self.check_passes(neighbor, route) > repeat_num:
                 movements.append(0)
+                distances.append(0)
                 continue
-            movements.append(1)   
-        return movements
+            movements.append(1)
+            if dist_map.all():
+                distances.append(dist_map[pose[1]][pose[0]])  
+        return movements, distances
     
     def calculate_distance_map(self):
         map = np.ones([self.width, self.height])
@@ -66,15 +73,74 @@ class CoveragePathPlanner():
             for j in range(self.height):
                 if self.tmp_map[i][j] != 1:
                     distance = [self.initial_pose[0] - i, self.initial_pose[1] -j]
-                    map[i][j] = np.round(np.linalg.norm(distance))
+                    map[i][j] = np.round(np.linalg.norm(distance)) + 2
         return map
-    
-    def plan(self):
+
+    def move_desicsion(self, movement, distances, dir="up"):
+        if dir in "up":
+            max_distance = 0
+            for counter, move in enumerate(movement):
+                if move:
+                    if distances[counter] > max_distance:
+                        max_distance = distances[counter]
+                        indx = counter
+            return indx
+        else:
+            min_distance = 100
+            for counter, move in enumerate(movement):
+                if move:
+                    if distances[counter] < min_distance:
+                        min_distance = distances[counter]
+                        indx = counter
+            return indx
+
+
+    def off_line_planning(self):
+        dist_map = self.calculate_distance_map()
         x = []
         y = []
         x.append(self.initial_pose[1])
         y.append(self.initial_pose[0])
-        movement = self.check_neighbors(self.initial_pose, [y, x])
+        steps = 0
+        repeat_num = 0
+        change_dir = 0
+        movement, distances = self.check_neighbors(self.initial_pose, [y, x], dist_map=dist_map)
+        while not self.done:
+            if not change_dir:
+                indx = self.move_desicsion(movement, distances)
+            else:
+                indx = self.move_desicsion(movement, distances, "down")
+            if indx == 0:              
+                x.append(x[-1] + 1)
+                y.append(y[-1] + 0)
+            if indx == 1:
+                x.append(x[-1] - 1)
+                y.append(y[-1] - 0)
+            if indx == 2:
+                x.append(x[-1] + 0)
+                y.append(y[-1] - 1)
+            if indx == 3:
+                x.append(x[-1] - 0)
+                y.append(y[-1] + 1)
+                
+            steps = steps + 1
+            movement, distances  = self.check_neighbors([y[-1], x[-1]], [y, x], repeat_num, dist_map=dist_map)
+
+            if all(dir == 0 for dir in movement):
+                repeat_num = repeat_num + 1
+                change_dir = not change_dir
+                movement, distances  = self.check_neighbors([y[-1], x[-1]], [y, x], repeat_num, dist_map=dist_map)
+
+            if not np.where(self.map == 0) or repeat_num > 3:
+                print(np.where(self.map == 0))
+                return x, y, steps
+
+    def on_line_planning(self):
+        x = []
+        y = []
+        x.append(self.initial_pose[1])
+        y.append(self.initial_pose[0])
+        movement, _ = self.check_neighbors(self.initial_pose, [y, x])
         repeat_num = 0
         steps = 0
         while not self.done:
@@ -83,31 +149,31 @@ class CoveragePathPlanner():
                 steps = steps + 1
                 x.append(x[-1] + 1)
                 y.append(y[-1] + 0)
-                movement = self.check_neighbors([y[-1], x[-1]], [y, x], repeat_num)
+                movement, _  = self.check_neighbors([y[-1], x[-1]], [y, x], repeat_num)
 
             while movement[2]:
                 steps = steps + 1
                 x.append(x[-1] + 0)
                 y.append(y[-1] - 1)
-                movement = self.check_neighbors([y[-1], x[-1]], [y, x], repeat_num)
+                movement, _  = self.check_neighbors([y[-1], x[-1]], [y, x], repeat_num)
 
             while movement[1]:
                 steps = steps + 1
                 x.append(x[-1] - 1)
                 y.append(y[-1] - 0)
-                movement = self.check_neighbors([y[-1], x[-1]], [y, x], repeat_num)
+                movement, _  = self.check_neighbors([y[-1], x[-1]], [y, x], repeat_num)
 
             while movement[3]:
                 steps = steps + 1
                 x.append(x[-1] - 0)
                 y.append(y[-1] + 1)
-                movement = self.check_neighbors([y[-1], x[-1]], [y, x], repeat_num)
+                movement, _  = self.check_neighbors([y[-1], x[-1]], [y, x], repeat_num)
                 if movement[1]:
                     break
 
             if all(dir == 0 for dir in movement):
                 repeat_num = repeat_num + 1
-                movement = self.check_neighbors([y[-1], x[-1]], [y, x], repeat_num)
+                movement, _  = self.check_neighbors([y[-1], x[-1]], [y, x], repeat_num)
 
             if not np.where(self.map == 0) or repeat_num > 2:
                 return x, y, steps
